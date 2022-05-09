1af113ca9c49541798a8ee188bdfb8c25d03cd09
xuedong <shang.xuedong@yahoo.fr>
2022-05-09 12:27:45 +0200

    Update beginner contest 250

--- コンテスト/AtCoder Beginner Contest 250/C - Adjacent Swaps/adjacent_swaps.py
+++ コンテスト/AtCoder Beginner Contest 250/C - Adjacent Swaps/adjacent_swaps.py
@@ -3,15 +3,17 @@
 
 if __name__ == "__main__":
     n, q = map(int, input().split())
-    nums = [i for i in range(1, n+1)]
+    nums = [i for i in range(n+1)]
+    inds = [i for i in range(n+1)]
 
     for i in range(q):
         num = int(input())
-        idx = nums.index(num)
-        if idx == n-1:
-            nums[idx], nums[idx-1] = nums[idx-1], nums[idx]
-        else:
-            nums[idx], nums[idx+1] = nums[idx+1], nums[idx]
+        
+        idx1 = inds[num]
+        idx2 = idx-1 if idx1 == n else idx+1
+        num1, num2 = nums[idx1], nums[idx2]
+          	
+        nums[idx1], nums[idx2] = nums[idx2], nums[idx1]
+        inds[num1], inds[num2] = inds[num2], inds[num1]
 
 	print(' '.join(map(str, nums)))
-
